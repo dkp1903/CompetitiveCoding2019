cc9d3dfdec299de5a33bd8c9bb442551180a470b
Dushyant Pathak <dushyantpathak6789@gmail.com>
2019-07-07 14:03:55 +0530

    Create IterativeInorder.cpp

---
+++ Tree/IterativeInorder.cpp
@@ -0,0 +1,29 @@
+//Iterative Inorder traversal
+
+#include<bits/stdc++.h>
+using namespace std;
+
+
+void inOrder(Node *root)
+{
+    if(!root)
+        return;
+        
+    stack<Node*>s;
+    Node *curr = root;
+    
+    while(curr && !s.empty())
+    {
+        while(curr)
+        {
+            s.push(curr);
+            curr = curr->left;
+        }
+        
+        curr = s.top();
+        s.pop();
+        
+        cout<<curr->data<<" ";
+        curr = curr->right;
+    }
+}
