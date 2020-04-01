5d42b6402a0f887c3af7d4cbb479f2ff5fcf3e4f
Dushyant Pathak <dushyantpathak6789@gmail.com>
2020-04-01 11:52:42 +0530

    Update RotateArray.cpp

--- Array/RotateArray.cpp
+++ Array/RotateArray.cpp
@@ -1,1 +1,17 @@
-
+  // Rotate Array a of size d by n elements.
+  /* Brute Force Method - 1
+   Time - O(n) || Aux Space - O(d)
+   - Store the first d elements in aux array
+   - Shift the remaining elements 1 by 1
+   - Add the initial elements in the end
+   */
+  /*
+  /* Method - 2
+  Time - O(n * d) || Aux Space - O(1)
+   - For every element of array, shift the elements 1 by 1, and add the initial element in the end
+   */
+  /* Method - 3 Juggling
+  Time - n || Space - 1
+  - 
+  */
+    
