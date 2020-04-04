a22146f8a03f23831af43520e4d7947661d73839
Dushyant Pathak <dushyantpathak6789@gmail.com>
2020-04-05 00:21:05 +0530

    Create MaxSumSubArraySizek.cpp

---
+++ Array/MaxSumSubArraySizek.cpp
@@ -0,0 +1,16 @@
+    // Max sum subarray of size k
+    int maxy(int k, vector<int>a)
+    {
+        int windowSum =0, maxSum = 0, windowStart = 0, windowEnd = 0, ans = 0;
+        for(windowEnd = 0 ; windowEnd < a.size(); windowEnd++)
+        {
+            windowSum += a[windowEnd];
+            if (windowEnd >= k - 1)
+            {
+                ans = max (ans, windowSum);
+                windowSum -= a[windowStart];
+                windowStart++;
+            }
+        }
+        return ans;
+    }
