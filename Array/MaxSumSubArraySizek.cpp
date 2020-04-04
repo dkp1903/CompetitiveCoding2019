8831f2f20f51270f9ef78e6bba2c670f90edf069
Dushyant Pathak <dushyantpathak6789@gmail.com>
2020-04-05 00:21:37 +0530

    Update MaxSumSubArraySizek.cpp

--- Array/MaxSumSubArraySizek.cpp
+++ Array/MaxSumSubArraySizek.cpp
@@ -1,4 +1,6 @@
     // Max sum subarray of size k
+    // Time : O(n)
+    //Space : O(1)
     int maxy(int k, vector<int>a)
     {
         int windowSum =0, maxSum = 0, windowStart = 0, windowEnd = 0, ans = 0;
